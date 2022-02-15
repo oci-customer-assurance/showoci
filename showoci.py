fc448fa45bfbd9b5b42d0019c13a894417a93e44
Adi Zohar <adizohar@gmail.com>
2022-02-15 09:12:57 -0500

    Add files via upload

--- showoci.py
+++ showoci.py
@@ -96,7 +96,7 @@ import sys
 import argparse
 import datetime
 
-version = "22.02.15"
+version = "22.02.22"
 
 ##########################################################################
 # check OCI version
